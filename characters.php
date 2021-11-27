c4b78efc9645251478dbdf2f8fadab8eba793084
Iliyan-Darediya <63348176+Iliyan-Darediya@users.noreply.github.com>
2021-11-26 16:57:20 -0800

    Add add function

--- characters.php
+++ characters.php
@@ -15,22 +15,22 @@ $characersArray = json_decode($json, true);
 //isset's to check if the values were changed
 if(isset($_GET['homer'])){
     //Call createDiv with the name and array parameter
-    createDiv('homer',$characersArray);       
+    echo createDiv('homer',$characersArray);       
 }
 if(isset($_GET['marge'])){
-    createDiv('marge',$characersArray);
+    echo createDiv('marge',$characersArray);
 }
 if(isset($_GET['bart'])){
-    createDiv('bart',$characersArray);
+    echo createDiv('bart',$characersArray);
 }
 if(isset($_GET['lisa'])){
-    createDiv('lisa',$characersArray);
+    echo createDiv('lisa',$characersArray);
 }
 if(isset($_GET['maggie'])){
-    createDiv('maggie',$characersArray);
+    echo createDiv('maggie',$characersArray);
 }
 if(isset($_GET['moe'])){
-    createDiv('moe',$characersArray);
+    echo createDiv('moe',$characersArray);
 }
 
 //createDiv will take the name and array as parameter and will output the character div in the html file
@@ -40,7 +40,7 @@ function createDiv($name,$array){
         //find the element we want to work with
         if($name == strtolower($character['first_name'])){
             //echo the image and info by getting the data from the provided array
-            echo '
+            $div = '
             <li class="characters__itemContainer">
                 <div class="characters__item">
                     <img 
@@ -52,20 +52,23 @@ function createDiv($name,$array){
                         <h3 class="characters__name">
                             '.$character['first_name'].' '.$character['last_name'].'
                         </h3>
-                    
-                        <div class="characters__age characters__attribute">
-                            <b>Age:</b>'.$character['age'].'
-                        </div>
-                        <div class="characters__occupation characters__attribute">
-                            <b>Occupation:</b>'.$character['occupation'].'
-                        </div>
-                        <div class="characters__voicedBy characters__attribute">
-                            <b>Voiced by:</b> '.$character['voiced_by'].'
-                        </div>
-                    </div>
-                </div>
-            </li>
             ';
+            $div.= add('age',$character,'Age');
+            $div.= add('occupation',$character,'Occupation');
+            $div.= add('voiced_by',$character,'Voiced by');
+            
+            
+            $div.='</div></div></li>';
+            return $div;
         }
+    }
+}
+function add($value,$array,$elementName){
+    if(array_key_exists($value,$array) && !empty($array[$value])){
+        return'
+        <div class="characters__occupation characters__attribute">
+            <b>'.$elementName.':</b>'.$array[$value].'
+        </div>
+        ';
     }
 }
